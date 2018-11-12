bd4c5f6a837990d34e94e5a253f710530f90fe48
“JyothiThipplikat” <lilmuthu4life21@gmail.com>
2018-11-11 23:00:14 -0600

    updated survey page

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="survey">
-    <!-- <h1>Begin your wedding planning!</h1>
+    <h1>Begin your wedding planning!</h1>
       <ul>
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
-      </ul>   -->
+      </ul>  
 
     <div class="form-group">
       <div v-for="priority in priorities">
         <h1>{{ priority.tag.name }}</h1>
-        <vue-slider v-on:input="OdacityOfHope(priority)" v-model="priority.percentage" ></vue-slider>
+        <vue-slider v-model="percentage"></vue-slider>
       </div>
-      <!-- <vue-slider input v-model="percentage"></vue-slider>
-      <vue-slider input v-model="percentage"></vue-slider>
-      <vue-slider input v-model="percentage"></vue-slider>   -->
+    
     </div> 
     <form v-on:submit.prevent="submit()">
       <div class="form-group">
@@ -26,29 +24,7 @@
     </form>
 
   </div>
-   <!-- <div class="tags-index">
-    <div class="container">
-      <form v-on:submit.prevent="submit()">
-        <h1>Begin your wedding planning!</h1>
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
-        <tr>
-        <div class="form-group">
-          <label for="Categories">Select Category</label>
-          <select class="form-control" id="Category" v-model="tagId">
-            <option v-for="tag in tags" v-if="tag.specialty" v-bind:value="tag.id"> {{tag.name}}</option>
-          </select>
-        </div>  
-        <div class="form-group">
-          <label>Select Priority:</label>
-          <input type="text" class="form-control" v-model="percentage">
-        </div> 
-        </tr>
-        <input type="submit" class="btn btn-primary" value="Submit">
-      </form>
-    </div>
-  </div> -->
+  
 </template>
 
 <style>
@@ -66,10 +42,7 @@ export default {
       percentage: "",
       tagId: 0,
       errors: [],
-      priorities: [],
-      currentPriorityId: 0,
-      changedPriorities: {},
-      timeStamp: null
+      priorities: []
     };
   },
   created: function() {
@@ -104,44 +77,8 @@ export default {
       .catch(error => {
         this.errors = error.response.data.errors;
       });
-      
-    // axios
-    //   .patch("http://localhost:3000/api/users/" + localStorage.getItem("user_id"), params)
-    //   .then(response => {
-
-        // this.$router.push("/customerprofile/" + response.data.id)
-
-    //   })
-    //   .catch(error => {
-    //     this.errors = error.response.data.errors;
-    // });
     },
-    OdacityOfHope: function(inputPriority) {
-      if (!this.changedPriorities[inputPriority.tag.name]) {
-        this.changedPriorities[inputPriority.tag.name] = true;
-      } else {
-        if (this.currentPriorityId !== inputPriority.id && this.$emit('myEvent').$children[0].moveEnd() === false) {
-          this.currentPriorityId = inputPriority.id;
-          console.log(inputPriority.tag.name);
-        }
-      // console.log(this.$emit('myEvent').$children[0].moveEnd());
-      }
-    },
-    updatePriority: function(inputPriority) {
-      console.log("updatePriority");
-      axios
-      .post("http://localhost:3000/api/change_priorities", {tag_id: inputPriority.tag.id, percentage: inputPriority.percentage})
-      .then(response => {
-        this.priorities = response.data;
-      });
-    }
   },
-  // watch: {
-  //   priorities: function(val, oldVal) {
-  //     var changedProperty = val.filter(function(i) {return oldVal.indexOf(i) < 0;})[0]; 
-  //     console.log(changedProperty);         
-  //   }
-  // },
   components: {
     vueSlider
   },
